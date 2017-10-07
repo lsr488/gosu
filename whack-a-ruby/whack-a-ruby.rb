bacded9ea9299be0d12fb46696a1e283fc8d1860
lsr488 <lsr488@gmail.com>
2017-10-07 19:44:20 -0400

    Ruby image alternates between visible and invisible.

--- whack-a-ruby/whack-a-ruby.rb
+++ whack-a-ruby/whack-a-ruby.rb
@@ -5,13 +5,14 @@ class WhackARuby < Gosu::Window
   def initialize 
     super(800, 600)
     self.caption = "Whack the Ruby!"
-    @image = Gosu::Image.new('ruby.png')
+    @ruby_image = Gosu::Image.new('ruby.png')
     @x = 200
     @y = 200
     @width = 50
     @height = 43
     @velocity_x = 5
     @velocity_y = 5
+    @visible = 0 #ruby is visible if positive, invisible if negative
   end #END initiliaze METHOD
 
   def update #move objects, user actions
@@ -19,10 +20,14 @@ class WhackARuby < Gosu::Window
     @y += @velocity_y
     @velocity_x *= -1 if @x + @width/2 > 800 || @x - @width/2 < 0
     @velocity_y *= -1 if @y + @height/2 > 600 || @y - @height/2 < 0
+    @visible -= 1 #subtracts 1 from @visible
+    @visible = 30 if @visible < -10 && rand < 0.01 #makes ruby visible for 30 frames if @visible 
   end #END update METHOD
 
   def draw
-    @image.draw(@x - @width / 2, @y - @width / 2, 1)
+    if @visible > 0
+      @ruby_image.draw(@x - @width / 2, @y - @width / 2, 1)
+    end
   end #END draw METHOD
 
 end # END WhackARuby CLASS
