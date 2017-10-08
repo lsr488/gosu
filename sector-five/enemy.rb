9e707a050111d50350ffee2ce2585b8fec4d750e
lsr488 <lsr488@gmail.com>
2017-10-08 13:55:54 -0400

    Enemy spite can enter at an angle, and respects left/right window edges.

--- sector-five/enemy.rb
+++ sector-five/enemy.rb
@@ -1,11 +1,14 @@
 class Enemy
   SPEED = 4
+  ANGLE = rand(-5..5)
 
   def initialize(window)
     @radius = 20
     @x = rand(window.width - 2 * @radius) + @radius
     @y = 0 # top of window
     @image = Gosu::Image.new('images/enemy.png')
+    @velocity_x = 0
+    @window = window
   end
 
   def draw
@@ -14,6 +17,19 @@ class Enemy
 
   def move
     @y += SPEED
+    @x += ANGLE
+    if @x > @window.width - @radius # right side
+      @velocity_x = 0
+      @x = @window.width - @radius
+    end
+    if @x < @radius # left side
+      @velocity_x = 0
+      @x = @radius
+    end
+    # if @y > @window.height - @radius # bottom edge
+    #   @velocity_y = 0
+    #   @y = @window.height - @radius
+    # end
   end
 
 end
