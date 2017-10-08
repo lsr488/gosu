f74bd432cba662ba2efb19e74beb4ade90bea760
lsr488 <lsr488@gmail.com>
2017-10-07 22:56:54 -0400

    Scores are archived and high scores are displayed at game over.

--- whack-a-ruby/whack-a-ruby.rb
+++ whack-a-ruby/whack-a-ruby.rb
@@ -1,6 +1,7 @@
 require 'gosu'
 
 class WhackARuby < Gosu::Window
+attr_accessor :sorted_high_scores
 
   def initialize 
     super(800, 600)
@@ -16,10 +17,13 @@ class WhackARuby < Gosu::Window
     @visible = 0 #ruby is visible if positive, invisible if negative
     @hit = 0 #tracks whether you hit the ruby or not
     @font = Gosu::Font.new(30) #writes text on screen, eg for keeping score
-    @score = 0
     @playing = true
     @start_time = 0
-    @game_duration = 15
+    @game_duration = 30
+    @score = 0
+    @for_score_records = 0
+    @high_scores = []
+    @sorted_high_scores = []    
   end #END initiliaze METHOD
 
   def update #move objects, user actions
@@ -31,7 +35,11 @@ class WhackARuby < Gosu::Window
       @visible -= 1 #subtracts 1 from @visible
       @visible = 30 if @visible < -10 && rand < 0.01 #makes ruby visible for 30 frames if @visible 
       @time_left = (@game_duration - ((Gosu.milliseconds - @start_time)/1000))
-      @playing = false if @time_left < 0
+      if @time_left < 0
+        update_score
+        sort_score
+        @playing = false
+      end
     end
   end #END update METHOD
 
@@ -52,8 +60,9 @@ class WhackARuby < Gosu::Window
     @font.draw(@score.to_s, 700, 20, 2)
     @font.draw(@time_left.to_s, 20, 20, 2)
     unless @playing
-      @font.draw("Game Over", 300, 300, 3)
-      @font.draw("Press Space Bar to Play Again", 175, 350, 3)
+      @font.draw("Game Over", 300, 200, 3) #x, y, ??
+      @font.draw("Press Space Bar to Play Again", 175, 250, 3)
+      @font.draw("HIGH SCORES: #{@sorted_high_scores.first(5).join(', ')}", 175, 300, 3)
       @visible = 20
     end
   end #END draw METHOD
@@ -78,6 +87,29 @@ class WhackARuby < Gosu::Window
       end
     end
   end # END button_down METHOD
+
+  def update_score 
+    @for_score_records = @score
+    filename = "scores.txt"
+    File.open(filename, 'a+') do |file|
+      file.write(@for_score_records)
+      file.write "\n"
+    end
+  end
+
+  def sort_score
+    filename = "scores.txt"
+    File.open(filename, 'a+').each_line do |line|
+      @high_scores << line.chomp.to_i
+    end
+    
+    @high_scores.each do |num|
+      num.to_s.rjust(2,'0')
+    end
+
+    @sorted_high_scores = @high_scores.sort.reverse
+    
+  end
 
 end # END WhackARuby CLASS
 
