9ede969e09cee4861f70416426f67eee0ad15669
lsr488 <lsr488@gmail.com>
2017-10-07 20:05:20 -0400

    Added score-keeping and a count-down timer.

--- whack-a-ruby/whack-a-ruby.rb
+++ whack-a-ruby/whack-a-ruby.rb
@@ -11,10 +11,12 @@ class WhackARuby < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
-    @velocity_x = 5
-    @velocity_y = 5
+    @velocity_x = 4
+    @velocity_y = 4
     @visible = 0 #ruby is visible if positive, invisible if negative
-    @hit = 0
+    @hit = 0 #tracks whether you hit the ruby or not
+    @font = Gosu::Font.new(30) #writes text on screen, eg for keeping score
+    @score = 0
   end #END initiliaze METHOD
 
   def update #move objects, user actions
@@ -24,7 +26,7 @@ class WhackARuby < Gosu::Window
     @velocity_y *= -1 if @y + @height/2 > 600 || @y - @height/2 < 0
     @visible -= 1 #subtracts 1 from @visible
     @visible = 30 if @visible < -10 && rand < 0.01 #makes ruby visible for 30 frames if @visible 
-
+    @time_left = (100 - (Gosu.milliseconds/1000))
   end #END update METHOD
 
   def draw
@@ -41,14 +43,18 @@ class WhackARuby < Gosu::Window
     end
     draw_quad(0, 0, screen_color, 800, 0, screen_color, 800, 600, screen_color, 0, 600, screen_color)
     @hit = 0
+    @font.draw(@score.to_s, 700, 20, 2)
+    @font.draw(@time_left.to_s, 20, 20, 2)
   end #END draw METHOD
 
   def button_down(id)
     if (id == Gosu::MsLeft)
       if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
         @hit = 1
+        @score += 5
       else
         @hit = -1
+        @score -= 1
       end
     end
   end # END button_down METHOD
