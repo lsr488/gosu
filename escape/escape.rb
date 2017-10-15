95ed8f4f731e4c8ed132cac7b53671b36c60be7b
lsr488 <lsr488@gmail.com>
2017-10-15 19:44:37 -0400

    Added boulders that fall.

--- escape/escape.rb
+++ escape/escape.rb
@@ -1,9 +1,11 @@
 require 'gosu'
 require 'chipmunk'
+require_relative 'boulder'
 
 class Escape < Gosu::Window
   DAMPING = 0.90
   GRAVITY = 400.0
+  BOULDER_FREQUENCY = 0.01
 
   attr_reader :space
 
@@ -12,9 +14,10 @@ class Escape < Gosu::Window
     self.caption = "Escape"
     @game_over = false
     @space = CP::Space.new
-    @background = Gosu::Image.new('images/background.png', tileable: true)    
+    @background = Gosu::Image.new('images/background.png', tileable: true)
     @space.damping = DAMPING
     @space.gravity = CP::Vec2.new(0.0, GRAVITY)
+    @boulders = []
   end
 
   def update
@@ -22,11 +25,18 @@ class Escape < Gosu::Window
       10.times do
         @space.step(1.0/600)
       end
-    end
+      if rand < BOULDER_FREQUENCY
+        @boulders.push Boulder.new(self, 200 + rand(400), -20)
+      end
+    end # end UNLESS game_over LOOP
   end
 
   def draw
-    
+    @background.draw(0,0,0)
+    @background.draw(0,529,0)
+    @boulders.each do |boulder|
+      boulder.draw
+    end
   end
 end
 
