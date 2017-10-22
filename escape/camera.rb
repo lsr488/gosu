e8bdda951e3313e1728e5a2925807130f2383107
lsr488 <lsr488@gmail.com>
2017-10-22 11:44:45 -0400

    Basic camera methods created.

--- escape/camera.rb
+++ escape/camera.rb
@@ -11,5 +11,19 @@ class Camera
     @y_offset_max = space_height - @window_height
   end
 
+  def center_on(sprite, right_margin, bottom_margin)
+    @x_offset = sprite.x - @window_width + right_margin
+    @y_offset = sprite.y - @window_height + bottom_margin
+    @x_offset = @x_offset_max if @x_offset > @x_offset_max
+    @x_offset = 0 if @x_offset < 0
+    @y_offset = @y_offset_max if @y_offset > @y_offset_max
+    @y_offset = 0 if @y_offset < 0
+  end
 
+  def view
+    @window.translate(-@x_offset, -@y_offset) do
+      yield
+    end
+  end
+  
 end # end CLASS
